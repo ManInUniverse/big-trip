408e68ee99d0466b3eb70eae3dec2a802943d241
Keks <keks@htmlacademy.ru>
2022-08-08 22:02:53 +0300

    :cat:

---
+++ src/render.js
@@ -0,0 +1,19 @@
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+const render = (component, container, place = RenderPosition.BEFOREEND) => {
+  container.insertAdjacentElement(place, component.getElement());
+};
+
+export {RenderPosition, createElement, render};
