e350df72c35edf80982c7a9999f8d73c95b6f31f
Artem Malyutin <81317413+ManInUniverse@users.noreply.github.com>
2022-08-25 15:21:33 +0500

    Изменения от 25.08.2022

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -1,6 +1,7 @@
 import { render } from '../render.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
-import NewTripEventView from '../view/new-trip-event-view.js';
+import TripEventCreatorView from '../view/trip-event-creator-view.js';
+import TripEventEditorView from '../view/trip-event-editor-view.js';
 import TripEventView from '../view/trip-event-view.js';
 
 export default class TripEventsPresenter {
@@ -10,7 +11,8 @@ export default class TripEventsPresenter {
     this.tripEventsContainer = tripEventsContainer;
 
     render(this.tripEventsListComponent, tripEventsContainer);
-    render(new NewTripEventView(), this.tripEventsListComponent.getElement());
+    render(new TripEventCreatorView(), this.tripEventsListComponent.getElement());
+    render(new TripEventEditorView(), this.tripEventsListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new TripEventView(), this.tripEventsListComponent.getElement());
--- src/view/trip-event-editor-view.js
+++ src/view/trip-event-editor-view.js
@@ -1,6 +1,6 @@
 import { createElement } from '../render.js';
 
-const createNewTripEventTemplate = () => `
+const createTripEventEditorTemplate = () => `
 <li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
     <header class="event__header">
@@ -67,7 +67,7 @@ const createNewTripEventTemplate = () => `
         <label class="event__label  event__type-output" for="event-destination-1">
           Flight
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -77,10 +77,10 @@ const createNewTripEventTemplate = () => `
 
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
         &mdash;
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
       </div>
 
       <div class="event__field-group  event__field-group--price">
@@ -88,11 +88,14 @@ const createNewTripEventTemplate = () => `
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-      <button class="event__reset-btn" type="reset">Cancel</button>
+      <button class="event__reset-btn" type="reset">Delete</button>
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
     </header>
     <section class="event__details">
       <section class="event__section  event__section--offers">
@@ -104,16 +107,16 @@ const createNewTripEventTemplate = () => `
             <label class="event__offer-label" for="event-offer-luggage-1">
               <span class="event__offer-title">Add luggage</span>
               &plus;&euro;&nbsp;
-              <span class="event__offer-price">30</span>
+              <span class="event__offer-price">50</span>
             </label>
           </div>
 
           <div class="event__offer-selector">
             <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
             <label class="event__offer-label" for="event-offer-comfort-1">
-              <span class="event__offer-title">Switch to comfort class</span>
+              <span class="event__offer-title">Switch to comfort</span>
               &plus;&euro;&nbsp;
-              <span class="event__offer-price">100</span>
+              <span class="event__offer-price">80</span>
             </label>
           </div>
 
@@ -148,26 +151,16 @@ const createNewTripEventTemplate = () => `
 
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-
-        <div class="event__photos-container">
-          <div class="event__photos-tape">
-            <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
-          </div>
-        </div>
+        <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
       </section>
     </section>
   </form>
 </li>
 `;
 
-export default class NewTripEventView {
+export default class TripEventEditorView {
   getTemplate() {
-    return createNewTripEventTemplate();
+    return createTripEventEditorTemplate();
   }
 
   getElement() {
