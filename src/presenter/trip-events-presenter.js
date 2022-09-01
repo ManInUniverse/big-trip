4057ff7fb1ad9ea72342c99e0067249d157a95fc
Artem Malyutin <81317413+ManInUniverse@users.noreply.github.com>
2022-09-01 18:07:08 +0500

    Изменения от 01.09.2022

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -1,6 +1,6 @@
 import { render } from '../render.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
-import AddEventView from '../view/add-event-view.js';
+// import AddEventView from '../view/add-event-view.js';
 import EditEventView from '../view/edit-event-view.js';
 import EventView from '../view/event-view.js';
 
@@ -20,11 +20,47 @@ export default class TripEventsPresenter {
     this.#offersByType = [...this.#tripEventsModel.offersByType];
 
     render(this.#tripEventsListComponent, this.#tripEventsContainer);
-    render(new AddEventView(this.#destinations, this.#offersByType), this.#tripEventsListComponent.element);
-    render(new EditEventView(this.#events[0], this.#destinations, this.#offersByType), this.#tripEventsListComponent.element);
+    // render(new AddEventView(this.#destinations, this.#offersByType), this.#tripEventsListComponent.element);
 
     for (let i = 0; i < this.#events.length; i++) {
-      render(new EventView(this.#events[i], this.#destinations, this.#offersByType), this.#tripEventsListComponent.element);
+      this.#renderEvent(this.#events[i], this.#destinations, this.#offersByType);
     }
+  };
+
+  #renderEvent = (event, destinations, offersByType) => {
+    const eventComponent = new EventView(event, destinations, offersByType);
+    const editEventComponent = new EditEventView(event, destinations, offersByType);
+
+    const replaceCardToForm = () => {
+      this.#tripEventsListComponent.element.replaceChild(editEventComponent.element, eventComponent.element);
+    };
+
+    const replaceFormToCard = () => {
+      this.#tripEventsListComponent.element.replaceChild(eventComponent.element, editEventComponent.element);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    eventComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceCardToForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    editEventComponent.element.querySelector('form').addEventListener('submit', () => {
+      replaceFormToCard();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+    editEventComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceFormToCard();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(eventComponent, this.#tripEventsListComponent.element);
   };
 }
