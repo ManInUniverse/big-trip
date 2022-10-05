0791e36649e8674a61b75c68dfa311a2afead55f
Artem Malyutin <malyutinartem92@gmail.com>
2022-10-05 12:50:03 +0300

    Доработы моедли дополнительных структур данных

--- src/model/offers-by-type-model.js
+++ src/model/offers-by-type-model.js
@@ -1,14 +1,26 @@
 import Observable from '../framework/observable.js';
+import { UpdateType } from '../const.js';
 
 export default class OffersByTypeModel extends Observable {
-  #offersByType = null;
+  #tripEventsApiService = null;
+  #offersByType = [];
 
-  constructor (offersByType) {
+  constructor (tripEventsApiService) {
     super();
-    this.#offersByType = offersByType;
+    this.#tripEventsApiService = tripEventsApiService;
   }
 
   get offersByType() {
     return this.#offersByType;
   }
+
+  init = async () => {
+    try {
+      this.#offersByType = await this.#tripEventsApiService.offersByType;
+    } catch(err) {
+      this.#offersByType = [];
+    }
+
+    this._notify(UpdateType.INIT);
+  };
 }
--- src/model/trip-events-model.js
+++ src/model/trip-events-model.js
@@ -1,54 +1,93 @@
 import Observable from '../framework/observable.js';
+import { UpdateType } from '../const.js';
 
 export default class TripEventsModel extends Observable {
-  #events = null;
+  #tripEventsApiService = null;
+  #events = [];
 
-  constructor (events) {
+  constructor (tripEventsApiService) {
     super();
-    this.#events = events;
+    this.#tripEventsApiService = tripEventsApiService;
   }
 
   get events() {
     return this.#events;
   }
 
-  updateEvent = (updateType, update) => {
+  init = async () => {
+    try {
+      const events = await this.#tripEventsApiService.events;
+      this.#events = events.map(this.#adaptToClient);
+    } catch(err) {
+      this.#events = [];
+    }
+
+    this._notify(UpdateType.INIT);
+  };
+
+  updateEvent = async (updateType, update) => {
     const index = this.#events.findIndex((event) => event.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting event');
     }
 
-    this.#events = [
-      ...this.#events.slice(0, index),
-      update,
-      ...this.#events.slice(index + 1),
-    ];
-
-    this._notify(updateType, update);
+    const response = await this.#tripEventsApiService.updateEvent(update);
+    try {
+      const updatedEvent = this.#adaptToClient(response);
+      this.#events = [
+        ...this.#events.slice(0, index),
+        updatedEvent,
+        ...this.#events.slice(index + 1)
+      ];
+      this._notify(updateType, updatedEvent);
+    } catch(err) {
+      throw new Error('Can\'t update event');
+    }
   };
 
-  addEvent = (updateType, update) => {
-    this.#events = [
-      update,
-      ...this.#events,
-    ];
-
-    this._notify(updateType, update);
+  addEvent = async (updateType, update) => {
+    try {
+      const response = await this.#tripEventsApiService.addEvent(update);
+      const newEvent = this.#adaptToClient(response);
+      this.#events = [newEvent, ...this.#events];
+      this._notify(updateType, newEvent);
+    } catch(err) {
+      throw new Error('Can\'t add event');
+    }
   };
 
-  deleteEvent = (updateType, update) => {
+  deleteEvent = async (updateType, update) => {
     const index = this.#events.findIndex((event) => event.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t delete unexisting event');
     }
 
-    this.#events = [
-      ...this.#events.slice(0, index),
-      ...this.#events.slice(index + 1),
-    ];
+    try {
+      await this.#tripEventsApiService.deleteEvent(update);
+      this.#events = [
+        ...this.#events.slice(0, index),
+        ...this.#events.slice(index + 1)
+      ];
 
-    this._notify(updateType);
+      this._notify(updateType);
+    } catch(err) {
+      throw new Error('Can\'t delete event');
+    }
+  };
+
+  #adaptToClient = (event) => {
+    const adaptedEvent = {...event,
+      basePrice: event['base_price'],
+      dateFrom: event['date_from'],
+      dateTo: event['date_to'],
+    };
+
+    delete adaptedEvent['base_price'];
+    delete adaptedEvent['date_from'];
+    delete adaptedEvent['date_to'];
+
+    return adaptedEvent;
   };
 }
